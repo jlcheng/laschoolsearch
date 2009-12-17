3d1832a800fffb73fab118d07f6c52e57a124796
John Cheng <johnlicheng@gmail.com>
2009-12-17 08:02:34 -0800

    Integrated frontend and backend, added caching.
    Baisc integration of the front and backend. The front end passes
    undisplayed addresses to fetch.do, the back end returns the schools
    back. A basic caching has also been added, but the caching system
    does not ever expire.

--- fetch.py
+++ fetch.py
@@ -5,33 +5,92 @@ import urllib2
 from BeautifulSoup import BeautifulSoup
 
 
+from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 
-class Address:
+
+SUFFIX_MAP = [
+    [['blvd','bl','boulevard'],'Bl'],
+    [['terrace','te','boulevard'],'Te'],
+    [['street','st'],'St'],
+    [['avenue','ave','av'],'Av'],
+    [['drive','dr'],'Dr'],
+    [['place','pl'],'Pl'],
+    [['plaza','pz'],'Pz'],
+    [['road','rd'],'Rd'],
+    [['lane','ln'],'Ln'],
+    [['way','wy'],'Wy']
+]
+def map_suffix(suffix):
+    suffix = suffix.lower()
+    for elem in SUFFIX_MAP:
+        if suffix in elem[0]:
+            return elem[1]
+    return suffix
+
+class Address():
     def __init__(self):
-        num = ''
-        street = ''
-        suffix = ''
+        self.num = None
+        self.street = None
+        self.suffix = None
+        self.schools = None
+        self.key = None
+
+    def setData(self, arr):
+        self.num = arr[0]
+        self.street = arr[1]
+        self.suffix = arr[2]
+        self.key = self.num + ':' + self.street + ':' + map_suffix(self.suffix)
         
     def __repr__(self):
-        return self.num + ' ' + self.street + ' ' + self.suffix
+        return self.num + ':' + self.street + ':' + self.suffix
 
+
+class AddressModel(db.Model):
+    num = db.StringProperty(required=True)
+    street = db.StringProperty(required=True)
+    suffix = db.StringProperty(required=True)
+    schools = db.StringListProperty(required=True)
+    addrKey = db.StringProperty(required=True)
+
+    def setData(self, arr):
+        self.num = arr[0]
+        self.street = arr[1]
+        self.suffix = arr[2]
+        self.addrKey = self.num + ':' + self.street + ':' + map_suffix(self.suffix)
+
+    def __repr__(self):
+        return self.num + ':' + self.street + ':' + self.suffix
 
 class FetchHandler(webapp.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/xml'
-        addresses = self.request.get('addresses')
-        addr = Address()
-        addr.num = '14142'
-        addr.street = 'Victory'
-        addr.suffix = 'Bl'
-        school = parseSchoolFinderResult(urllib2.urlopen(toURL(addr)))
-        logging.info(school)
-        if hasText(addresses):
+        
+        model = {}
+        if hasText(self.request.get('addresses')):
             path = os.path.join(os.path.dirname(__file__), 'fetch.htm')
-            self.response.out.write(template.render(path, {'addresses': addresses.split('|')}))
+            addrList = []
+            for elem in self.request.get('addresses').split('|'):
+                addr = Address()
+                addr.setData(elem.split(':'))
+                addrModel = db.GqlQuery('SELECT * FROM AddressModel WHERE addrKey = :1 LIMIT 1', addr.key).get()
+                if addrModel:
+                    logging.debug('address cached: %s' % addr)
+                    addr.schools = addrModel.schools
+                else:
+                    url = toURL(addr)
+                    logging.debug('address not cached: %s' % addr)
+                    logging.debug('sending request to %s' % url)
+                    addr.schools = parseSchoolFinderResult(urllib2.urlopen(url))
+                    AddressModel(num=addr.num,
+                                 street=addr.street,
+                                 suffix=addr.suffix,
+                                 schools=addr.schools,
+                                 addrKey=addr.key).put()
+                addrList.append(addr)
+            self.response.out.write(template.render(path, {'addresses': addrList}))
         else:
             path = os.path.join(os.path.dirname(__file__), 'fetch.htm')
             self.response.out.write(template.render(path, {}))
@@ -45,16 +104,14 @@ def parseSchool(school):
 def parseSchoolFinderResult(page):
     soup = BeautifulSoup(''.join(page.readlines()))
     if re.search('NO MATCH WAS FOUND FOR ', str(soup)):
-        return 'NO MATCH WAS FOUND'
-    retval = ''
+        return ['NO MATCH FOUND']
+    retval = []
     for school in soup.findAll('table')[0]('tr'):
-        if retval != '':
-            retval = retval + '\n'
-        retval = retval + parseSchool(school) 
+        retval.append(parseSchool(school))
     return retval
 
 def toURL(address):
-    return 'http://search.lausd.k12.ca.us/cgi-bin/fccgi.exe?Direction=(none)&w3exec=schfinder2&Submit=Submit&StreetName=%s&StreetNumber=%s&Suffix=%s' % ( address.street, address.num, address.suffix )
+    return 'http://search.lausd.k12.ca.us/cgi-bin/fccgi.exe?Direction=(none)&w3exec=schfinder2&Submit=Submit&StreetName=%s&StreetNumber=%s&Suffix=%s' % ( address.street, address.num, map_suffix(address.suffix) )
 
 def hasText(str):
     """
@@ -63,6 +120,7 @@ def hasText(str):
     return str != None and len(str.strip()) != 0
 
 def main():
+    logging.getLogger().setLevel(logging.DEBUG)
     urlmap = [
         # Maps /fetch.do -> FetchHandler
         ( '/fetch.do', FetchHandler ),
@@ -75,6 +133,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-
-
-# "C:\Program Files"\Google\google_appengine\dev_appserver.py
